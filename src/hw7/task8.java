0f1f683bdb69307b8780041ae976bcd53e5e726e
Poddubniy <Poddubniy@gmail.com>
2015-10-26 17:56:03 +0200

    dfgasdfsfdg

--- src/hw7/task8.java
+++ src/hw7/task8.java
@@ -5,22 +5,25 @@ package hw7;
  */
 public class task8 {
     public static void main(String[] args) {
+        int x=0;
         long start = System.currentTimeMillis();
         for (int i = 0; i<1000000; i++) {
-            int x = 123456 >> 2;
+            x = 123456789 << 1;
         }
         long end = System.currentTimeMillis();
 
         System.out.println("Took ms: " + (end - start));
 
+        int y=0;
         long start2 = System.currentTimeMillis();
         for (int i = 0; i<1000000; i++) {
-            int y = 123456 * 2;
+            y = 123456789 * 2;
         }
         long end2 = System.currentTimeMillis();
 
         System.out.println("Took ms: " + (end2 - start2));
 
+        System.out.println(x+" "+y);
 
     }
 }
