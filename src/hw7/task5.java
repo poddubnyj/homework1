be45d8ac357e96fe3b1ea2bcfd994ce04c6731ce
Poddubniy <Poddubniy@gmail.com>
2015-11-12 11:41:16 +0200

    dfgasdfsfdg

--- src/hw7/task5.java
+++ src/hw7/task5.java
@@ -1,19 +1,23 @@
 package hw7;
 
+import java.util.Random;
+
 /**
  * Created by user on 24.10.2015.
  */
 public class task5 {
     public static void main(String[] args) {
-        int n = (int) (Math.random()*10);
-        int [][] I = new int[8][n];
+        Random r = new Random();
+        int [][] I = new int[8][];
         for(int i =0; i<I.length; i++){
-            for(int j=0; j<I[i].length; j++){
 
-                I[i][j]=10 +(int)(Math.random()*((99-10)+1));
-                System.out.print(I[i][j] + "\t");
+            I[i]= new int[r.nextInt(10)+1];
+            for (int j = 0; j<I[i].length; j++){
+                I[i][j] = r.nextInt(10);
+                System.out.print(I[i][j]+"\t");
             }
             System.out.println();
+
         }
 
     }
